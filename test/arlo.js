4e2814579240551bb10788435dcb8813931cfa16
roylines <roy@roylines.co.uk>
2018-11-28 16:42:39 +0000

    added mode setting

---
+++ test/arlo.js
@@ -0,0 +1,45 @@
+const arlo = require('../index.js');
+const should = require('chai').should();
+
+const auth = {
+  email: process.env.ARLO_USER,
+  password: process.env.ARLO_PASSWORD
+};
+
+describe('arlo', () => {
+  
+  before(async () => {
+    await arlo.login(auth);
+  });
+
+  after(async() => {
+    await arlo.logout();
+  });
+
+  it('can set mode', async () => {
+    let active = await arlo.modes.active();
+    
+    await arlo.modes.set('mode1');
+    let newActive = await arlo.modes.active();
+    
+    await arlo.modes.set(active);
+    newActive.should.equal('mode1');
+  });
+
+  it('can get active mode', async () => {
+    const mode = await arlo.modes.active();
+    mode.should.have.string('mode');
+  });
+  
+  it('can get devices', async () => {
+    const devices = await arlo.devices.get();
+    devices.should.be.an('array');
+    devices.should.not.be.empty;
+  });
+  
+  it('can get basestation', async () => {
+    const basestation = await arlo.basestation.get();
+    basestation.should.have.property('deviceType');
+    basestation.deviceType.should.equal('basestation');
+  });
+});
